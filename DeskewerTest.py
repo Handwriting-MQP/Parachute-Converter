5bcfc2bd988e6edfde7e51d69174393ca623898a
hmpatel1350 <hmpatel@wpi.edu>
2023-09-07 11:31:12 -0400

    Added deskewed files

--- DeskewerTest.py
+++ DeskewerTest.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 from skimage import io
 from skimage.color import rgb2gray
@@ -5,8 +7,24 @@ from skimage.transform import rotate
 
 from deskew import determine_skew
 
-image = io.imread('input.png')
-grayscale = rgb2gray(image)
-angle = determine_skew(grayscale)
-rotated = rotate(image, angle, resize=True) * 255
-io.imsave('output.png', rotated.astype(np.uint8))
+input_folder = 'SampleDocument_PNG'
+output_folder = 'DeskewedImages'
+
+
+def convert_image(file_name):
+    image = io.imread(input_folder + '/' + file_name)
+    grayscale = rgb2gray(image)
+    angle = determine_skew(grayscale)
+    rotated = rotate(image, angle, resize=True, cval=1) * 255
+    io.imsave(output_folder + '/' + file_name, rotated.astype(np.uint8))
+
+
+if __name__ == '__main__':
+    isExist = os.path.exists(output_folder)
+    if not isExist:
+        os.makedirs(output_folder)
+
+    skewed_files = os.listdir('SampleDocument_PNG')
+
+    for file_name in skewed_files:
+        convert_image(file_name)
