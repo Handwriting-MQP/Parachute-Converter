2515ca0674782a62a9216c590f239db4366a7018
liamhall547 <llhall@wpi.edu>
2024-01-23 12:08:37 -0500

    Update HandwritingRecognitionPipeline.py

--- HandwritingRecognitionPipeline.py
+++ HandwritingRecognitionPipeline.py
@@ -89,6 +89,7 @@ def process_handwriting_data(pdfs_dir, gui_queue):
         for pdf in data_pdfs:
             cur_pdf_dir = pdf.replace(".pdf", "")
             temporary_pdf_image_dirs.append(cur_pdf_dir)
+            print("    Created temporary directory for: " + cur_pdf_dir)
 
             # Here, "tmp_dir_name" is the top-level temporary directory, whereas
             # "cur_pdf_dir" is the temporary directory for the particular data PDF.
@@ -100,7 +101,6 @@ def process_handwriting_data(pdfs_dir, gui_queue):
             split_pdf_into_images(path_to_cur_pdf, output_path_for_cur_pdfs_images)
 
         for temporary_pdf_image_dir in temporary_pdf_image_dirs:
-            print("Created temporary directory: " + temporary_pdf_image_dir)
 
             # These first two code blocks make the results folders for each data PDF.
 
